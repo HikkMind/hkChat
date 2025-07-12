336236a473accf94e3c5a672eaa37c09c4ca7cf0
HikkMind <liferisinghigh@yandex.ru>
2025-07-12 17:08:18 +0300

    add message time

--- server/tables/database.go
+++ server/tables/database.go
@@ -1,14 +1,17 @@
 package tables
 
+import "time"
+
 type User struct {
-	ID       uint   `gorm:"PrimaryKey;autoIncrement"`
-	Username string `gorm:"unique;size:20;not null"`
-	Password string `gorm:"not null"`
-	Messages string `gorm:"foreignKey:SenderID;references:ID;constraint:OnDelete:CASCADE"`
+	ID       uint      `gorm:"PrimaryKey;autoIncrement"`
+	Username string    `gorm:"unique;size:20;not null"`
+	Password string    `gorm:"not null"`
+	Messages []Message `gorm:"foreignKey:SenderID;references:ID;constraint:OnDelete:CASCADE"`
 }
 
 type Message struct {
-	ID       uint `gorm:"PrimaryKey;autoIncrement"`
-	SenderID uint
-	Message  string
+	ID        uint `gorm:"PrimaryKey;autoIncrement"`
+	SenderID  uint
+	Message   string
+	CreatedAt time.Time
 }
